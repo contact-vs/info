dc5a23bc8afdcaa390ae1e9ad9b2d52f9e9fd9d5
contact-vs <111386859+contact-vs@users.noreply.github.com>
2022-09-20 15:14:54 +0300

    Update SimpleFileWalker.java

--- code_samples/java/SimpleFileWalker.java
+++ code_samples/java/SimpleFileWalker.java
@@ -11,25 +11,24 @@ public class SimpleFileWalker {
         int nFiles = 0;
         long totalSize = 0L;
         try (Stream<Path> paths = Files.walk(Paths.get(args[0]))) {
-            Iterable< Path > iterable = paths::iterator;
-            for ( Path s : iterable )
-            {
-                if( Files.isDirectory(s, LinkOption.NOFOLLOW_LINKS) ) {
+            Iterable<Path> iterable = paths::iterator;
+            for (Path s : iterable) {
+                if (Files.isDirectory(s, LinkOption.NOFOLLOW_LINKS)) {
                     ++nDirs;
-                    System.out.println( "Directory N" + nDirs + " = " + s );
+                    System.out.println("Directory N" + nDirs + " = " + s);
                 }
-                if( Files.isRegularFile(s, LinkOption.NOFOLLOW_LINKS) ) {
+                if (Files.isRegularFile(s, LinkOption.NOFOLLOW_LINKS)) {
                     ++nFiles;
-                    System.out.println( "File N" + nFiles+ " = " + s );
-                    totalSize+=Files.size(s);
+                    System.out.println("File N" + nFiles + " = " + s);
+                    totalSize += Files.size(s);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println( "-------------------------------" );
-        System.out.println( "Total directories=" + nDirs );
-        System.out.println( "Total files=" + nFiles );
-        System.out.println( "Total size=" + totalSize );
+        System.out.println("-------------------------------");
+        System.out.println("Total directories=" + nDirs);
+        System.out.println("Total files=" + nFiles);
+        System.out.println("Total size=" + totalSize);
     }
 }
